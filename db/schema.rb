2f423e1b00db76a75019698f1bbde21bf2c1772d
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-02-07 18:49:31 -0500

    Add feri images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207185834) do
+ActiveRecord::Schema.define(version: 20170207192532) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_categories_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
